d9225c5693d96308cead95a0600a99fd9253cf58
Cc <li_chao_cheng@163.com>
2022-11-24 23:18:28 +0800

    update: other

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,47 +1,31 @@
 <template>
   <div>
-    <el-table :data="tableData" stripe border style="width: 100%">
-      <el-table-column prop="id" label="Id" width="50"/>
-      <el-table-column prop="name" label="Name" v-show="true" width="180"/>
-      <el-table-column prop="sex" label="Sex" width="50"/>
-      <el-table-column prop="email" label="Email" width="180"/>
-      <el-table-column prop="images_url" label="Images"/>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="id" label="Id" width="50" />
+      <el-table-column prop="name" label="Name" v-show="true" width="180" />
+      <el-table-column prop="sex" label="Sex" width="50" />
+      <el-table-column prop="email" label="Email" width="180" />
+      <el-table-column prop="images_url" label="Images" />
     </el-table>
     <!--    <el-pagination background :small="false" :total="1000"/>-->
   </div>
 </template>
 
 <script>
-import {userList} from "@/api/user";
+import { userList } from "@/api/user"
 
 export default {
   name: "UserIndex",
-  data() {
+  data () {
     return {
       tableData: []
     }
   },
-  created() {
-    Date.prototype.Format = function (fmt) {
-      const o = {
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "H+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds() //毫秒
-      };
-      if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-      for (const k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-      return fmt;
-    }
-    this.initTable()
+  created () {
+    this.initUserList()
   },
   methods: {
-    initTable() {
-
+    initUserList () {
       userList(1, 10).then(response => {
         console.log("用户", response)
         if (response.data.code === -1) {
--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -3,22 +3,12 @@
     <el-form ref="loginFrom" :model="loginForm" label-width="50px">
       <h1 class="center">Login</h1>
       <el-form-item prop="username">
-        <el-input
-            ref="username"
-            v-model="loginForm.username"
-            placeholder="Username"
-            type="text"
-            tabindex="1"
-            autocomplete="on"/>
+        <el-input ref="username" v-model="loginForm.username" placeholder="Username" type="text" tabindex="1"
+          autocomplete="on" />
       </el-form-item>
       <el-form-item prop="username">
-        <el-input
-            ref="password"
-            v-model="loginForm.password"
-            placeholder="Password"
-            type="password"
-            tabindex="2"
-            autocomplete="on"/>
+        <el-input ref="password" v-model="loginForm.password" placeholder="Password" type="password" tabindex="2"
+          autocomplete="on" />
       </el-form-item>
       <el-form-item class="center">
         <el-button type="primary" @click.prevent="login">Login</el-button>
@@ -29,9 +19,11 @@
 </template>
 
 <script>
+import { connectWebsocket } from "@/api/websocket/socket"
+import { ElNotification } from 'element-plus'
 export default {
   name: "LoginInfo",
-  data() {
+  data () {
     return {
       loginForm: {
         username: 'Super',
@@ -39,30 +31,63 @@ export default {
       }
     }
   },
+  created () {
+
+  },
   methods: {
-    login() {
+    connWS () {
+      connectWebsocket(
+        "ws://172.12.12.188:8887?uid=1",
+        // 发送
+        "HeartBeat",
+        // 成功拿到后台返回的数据的回调函数
+        (data) => {
+          ElNotification({ title: 'WebSocket 连接成功', message: "Cc 上线了", type: 'success', duration: 3500 })
+          console.log("成功的回调函数, 接收到的data数据： ", data)
+        },
+        // websocket连接失败的回调函数
+        () => {
+
+          ElNotification({
+            title: 'WebSocket 失败',
+            message: "请检查服务器",
+            type: 'error',
+            duration: 2000,
+          })
+          console.log("失败的回调函数")
+        }
+      )
+    },
+    message (messages) {
+      ElNotification({ title: 'Online Message', message: messages, type: 'success', duration: 10000 })
+    },
+    login () {
+      // todo test websocket server
       let p = this.loginForm
       if (p.username === "") return this.$message.error("username  不能为空")
       if (p.password === "") return this.$message.error("password  不能为空")
-      let params = {name_and_email: p.username, password: p.password}
+      let params = { name_and_email: p.username, password: p.password }
       this.$store.dispatch('user/login', params).then(() => {
         this.$message.success("欢迎👏 " + p.username + " 登录成功")
-        this.$router.push({path: this.redirect || '/home', query: this.otherQuery})
+        // 这里连接websocket服务
+        this.connWS()
+        this.message("Cc：" + p.username + "您终于上线了，盯你很久了，更加开发完成✅Cc_App Server And Client")
+        // this.message("Cc 上线了")
+        // this.message("HyperLi 上线了")
+        // this.message("SuperLi 上线了")
+        this.$router.push({ path: this.redirect || '/home', query: this.otherQuery })
       }).catch(error => {
         this.$message.error("Login" + error)
       })
     },
-    resetForm() {
-      this.loginForm = {username: "", password: ""}
+    resetForm () {
+      this.loginForm = { username: "CcSupper", password: "Cc@cclcc123" }
     }
   },
   computed: {}
 }
 </script>
-
-
 <style lang="scss">
-
 .center {
   margin: auto;
   width: 60%;
